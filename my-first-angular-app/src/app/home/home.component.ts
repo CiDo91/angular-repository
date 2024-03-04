66381f4b98e16d539e8bbbe19a0698540616675a
DodoSoftwareEngineer <ciprian_dorofte@yahoo.com>
2024-03-04 17:40:58 +0000

    Hour 2:13

--- my-first-angular-app/src/app/home/home.component.ts
+++ my-first-angular-app/src/app/home/home.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
   ngOnInit() {
     this.productsService
       .getProducts('https://zany-giggle-vp66j9944wj3qjr-3000.app.github.dev/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products);
+        this.products = products.items;
       });
+  }
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
   }
 }
