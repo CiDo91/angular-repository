d5151b4d66346ce73b714ecb5cfa6a56f1f38d72
DodoSoftwareEngineer <ciprian_dorofte@yahoo.com>
2024-03-07 17:52:48 +0000

    Add pagination

--- my-first-angular-app/src/app/home/home.component.ts
+++ my-first-angular-app/src/app/home/home.component.ts
@@ -3,24 +3,34 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  totalRecords: number = 0;
+  rows: number = 5;
   products: Product[] = [];
   ngOnInit() {
-    this.productsService
-      .getProducts('https://zany-giggle-vp66j9944wj3qjr-3000.app.github.dev/clothes', { page: 0, perPage: 5 })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-      });
+    this.fetchProducts(0, this.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService.getProducts('https://zany-giggle-vp66j9944wj3qjr-3000.app.github.dev/clothes', {page, perPage}).subscribe((products: Products) => {
+      this.products = products.items;
+      this.totalRecords = products.total;
+    });
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
   }
 
   onProductOutput(product: Product) {
